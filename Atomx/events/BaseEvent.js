69ee0235fb6194c7a083816c5c3aad8f8b912661
Doc <65479309+DocilElm@users.noreply.github.com>
2024-02-27 23:06:56 -0800

    Add custom events & default to vanilla events
    Add custom events & default to vanilla events
    added a couple custom events (the ones starting with player _should_ all be client sent packets i might break this later cuz im like that) also defaulted to vanilla ct events if the event name wasnt found & added params to the callback fn

--- Atomx/events/BaseEvent.js
+++ Atomx/events/BaseEvent.js
@@ -1,3 +1,9 @@
+import { TextHelper } from "../helper/Text"
+
+// Constant used to get the packet's ENUMS
+// and also filter the class in packetRecieved event
+const S38PacketPlayerListItem = net.minecraft.network.play.server.S38PacketPlayerListItem
+
 /**
  * - BaseEvent class to create events with profiling features integrated into them
  * @class
@@ -23,10 +29,10 @@ export default class BaseEvent {
      * - Creates the event for this class
      */
     _create() {
-        const profilingFn = () => {
+        const profilingFn = (...params) => {
             this.timesRan += 1
 
-            this.callbackFn()
+            this.callbackFn(...params)
 
             if (this.properties.debugMode) ChatLib.chat(`&c[&4Atomx&c]&r &aDebugging &b${this.featureName} &aEvent: &b${this.name} &ahas ran &7x${this.timesRan} &afor &6${(this.stoppedAt ?? Date.now()) - (this.startedAt ?? Date.now())}ms`)
         }
@@ -35,17 +41,172 @@ export default class BaseEvent {
         if (!("registerWhen" in this.properties)) this.properties.registerWhen = () => World.isLoaded()
 
         switch (this.name) {
+            // "Vanilla" events
             case "step":
                 if ("delay" in this.properties) return this.event = register("step", profilingFn.bind(this)).setDelay(this.properties.delay).unregister()
-                if ("fps" in this.properties) return this.event = register("step", profilingFn.bind(this)).setDelay(this.properties.fps).unregister()
+                if ("fps" in this.properties) return this.event = register("step", profilingFn.bind(this)).setFps(this.properties.fps).unregister()
+
                 break
 
-            case "tick":
-                this.event = register("tick", profilingFn.bind(this)).unregister()
+            case "soundplay":
+                this.event = register("soundPlay", profilingFn.bind(this)).setCriteria(this.properties.criteria).unregister()
+                
+                break
+
+            // Custom events
+            case "scoreboardpacket":
+                this.event = register("packetRecieved", (packet, rEvent) => {
+                    const channel = packet.func_149307_h()
+                    if (channel !== 2) return
+
+                    const teamStr = packet.func_149312_c()
+                    const teamMatch = teamStr.match(/^team_(\d+)$/)
+                    if (!teamMatch) return
+
+                    const formatted = packet.func_149311_e().concat(packet.func_149309_f())
+                    const unformatted = formatted.removeFormatting()
+
+                    if (!unformatted) return
+
+                    TextHelper.matchesCriteria(profilingFn.bind(this), this.properties.criteria, unformatted, rEvent, formatted)
+                }).setFilteredClass(net.minecraft.network.play.server.S3EPacketTeams).unregister()
+
+                break
+
+            case "tabupdatepacket":
+                this.event = register("packetReceived", (packet, rEvent) => {
+                    const players = packet.func_179767_a() // .getPlayers()
+                    const action = packet.func_179768_b() // .getAction()
+                
+                    if (action !== S38PacketPlayerListItem.Action.UPDATE_DISPLAY_NAME) return
+                
+                    players.forEach(addPlayerData => {
+                        const name = addPlayerData.func_179961_d() // .getDisplayName()
+                        
+                        if (!name) return
+                
+                        const formatted = name.func_150254_d() // .getFormattedText()
+                        const unformatted = formatted.removeFormatting()
+                    
+                        if (action !== S38PacketPlayerListItem.Action.UPDATE_DISPLAY_NAME) return
+                
+                        TextHelper.matchesCriteria(profilingFn.bind(this), this.properties.criteria, unformatted, rEvent, formatted)
+                    })
+                }).setFilteredClass(S38PacketPlayerListItem).unregister()
+
+                break
+
+            case "tabaddpacket":
+                this.event = register("packetReceived", (packet, rEvent) => {
+                    const players = packet.func_179767_a() // .getPlayers()
+                    const action = packet.func_179768_b() // .getAction()
+        
+                    if (action !== S38PacketPlayerListItem.Action.ADD_PLAYER) return
+        
+                    players.forEach(addPlayerData => {
+                        const name = addPlayerData.func_179961_d() // .getDisplayName()
+                        
+                        if (!name) return
+        
+                        const formatted = name.func_150254_d() // .getFormattedText()
+                        const unformatted = formatted.removeFormatting()
+                    
+                        if (action !== S38PacketPlayerListItem.Action.ADD_PLAYER) return
+        
+                        TextHelper.matchesCriteria(profilingFn.bind(this), this.properties.criteria, unformatted, rEvent, formatted)
+                    })
+                }).setFilteredClass(S38PacketPlayerListItem).unregister()
+
+                break
+
+            case "tabfooterpacket":
+                this.event = register("packetReceived", (packet, rEvent) => {
+                    packet.func_179701_b()?.func_150253_a()?.forEach(chatComponent => {
+                        const formatted = chatComponent.func_150254_d()
+                        const unformatted = formatted?.removeFormatting()
+        
+                        TextHelper.matchesCriteria(profilingFn.bind(this), this.properties.criteria, unformatted, rEvent, formatted)
+                    })
+                }).setFilteredClass(net.minecraft.network.play.server.S47PacketPlayerListHeaderFooter).unregister()
+
+                break
+
+            case "windowitemspacket":
+                this.event = register("packetReceived", (packet) => {
+                    profilingFn(packet.func_148910_d())
+                }).setFilteredClass(net.minecraft.network.play.server.S30PacketWindowItems).unregister()
+
+                break
+
+            case "windowopenpacket":
+                this.event = register("packetReceived", (packet) => {
+                    const windowTitle = packet.func_179840_c().func_150254_d().removeFormatting()
+                    const windowID = packet.func_148901_c()
+                    const hasSlots = packet.func_148900_g()
+                    const slotCount = packet.func_148898_f()
+                    const guiID = packet.func_148902_e()
+                    const entityID = packet.func_148897_h()
+                
+                    profilingFn(windowTitle, windowID, hasSlots, slotCount, guiID, entityID)
+                }).setFilteredClass(net.minecraft.network.play.server.S2DPacketOpenWindow).unregister()
+
+                break
+
+            case "chatpacket":
+                this.event = register("packetReceived", (packet, rEvent) => {
+                    // Check if the packet is for the actionbar
+                    if (packet.func_148916_d()) return
+        
+                    const chatComponent = packet.func_148915_c()        
+                    const formatted = chatComponent?.func_150254_d()
+                    const unformatted = formatted?.removeFormatting()
+                
+                    if (!unformatted) return
+                    
+                    TextHelper.matchesCriteria(profilingFn.bind(this), this.properties.criteria, unformatted, rEvent, formatted)
+                }).setFilteredClass(net.minecraft.network.play.server.S02PacketChat).unregister()
+
+                break
+
+            case "actionbarpacket":
+                this.event = register("packetReceived", (packet, rEvent) => {
+                    // Check if the packet is for the actionbar
+                    if (!packet.func_148916_d()) return
+        
+                    const chatComponent = packet.func_148915_c()        
+                    const formatted = chatComponent?.func_150254_d()
+                    const unformatted = formatted?.removeFormatting()
+                    
+                    if (!unformatted) return
+                    
+                    TextHelper.matchesCriteria(profilingFn.bind(this), this.properties.criteria, unformatted, rEvent, formatted)
+                }).setFilteredClass(net.minecraft.network.play.server.S02PacketChat).unregister()
+
+                break
+
+            case "playerblockplacementpacket":
+                this.event = register("packetSent", (packet) => {
+                    const position = packet.func_179724_a()
+                    const blockPosition = new BlockPos(position)
+                
+                    const [ x, y, z ] = [blockPosition.x, blockPosition.y, blockPosition.z]
+                    const ctBlock = World.getBlockAt(x, y, z)
+        
+                    profilingFn(ctBlock, [x, y, z], blockPosition)
+                }).setFilteredClass(net.minecraft.network.play.client.C08PacketPlayerBlockPlacement).unregister()
+
+                break
+
+            case "playerclickwindowpacket":
+                this.event = register("packetSent", (packet) => {
+                    // Container name, Slot clicked
+                    profilingFn(Player.getContainer().getName(), packet.func_149544_d())
+                }).setFilteredClass(net.minecraft.network.play.client.C0EPacketClickWindow).unregister()
+
                 break
         
             default:
-                throw new Error(`Event with name ${this.name} isn't in the list`)
+                this.event = register(this.name, profilingFn.bind(this)).unregister()
         }
     }
 
